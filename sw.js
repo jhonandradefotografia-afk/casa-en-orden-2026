bff1f40c6f30009ddf5e15ded9ab38a9f1ac69aa
jhonandradefotografia-afk <jhonandradefotografia@gmail.com>
2025-12-30 22:04:43 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const defaultState = {
-  couple: "Jhon & Daniela",
-  purpose: "Este 2026 administramos con sabiduría lo que Dios nos confía. Caminamos en unidad, con paz, sin ansiedad, y con disciplina para honrar a Dios, cubrir nuestra casa y avanzar hacia nuestras metas.",
-  movs: [],
-  customVerses: [],
-  goals: {
-    emergencyTarget: 0,
-    tripsTarget: 0,
-    debtTarget: 0
-  }
-};
-
 
